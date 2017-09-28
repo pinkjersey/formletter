7fbfa6bd7eb33ded02623f1866e1c9f55fc234cb
Mustafa Ozturk <mdozturk@yahoo.com>
2017-09-27 21:56:34 -0400

    Some additional work on the form letter

--- app/app.js
+++ app/app.js
@@ -11,56 +11,42 @@ angular.module('myApp', [
 
     $routeProvider.otherwise({redirectTo: '/view1'});
 }]).controller('AppCtrl', function AppController($scope, $rootScope, $http, $compile) {
-    angular.element(document).ready(function () {
-        var converter = new Markdown.Converter();
-        $http({
-            method: 'GET',
-            url: 'http://localhost:8080/templateServer/'
-        }).then(function successCallback(response) {
-            var h = converter.makeHtml(response.data);
+        angular.element(document).ready(function () {
+            var converter = new showdown.Converter();
+            converter.setOption('tables', 'true');
+            var templateText = "<div style=\"text-align: right\">{{name}}</div>\n" +
+                "<div style=\"text-align: right\">{{address1}}</div>" +
+                "<div style=\"text-align: right\">{{address2}}</div>\n" +
+                "{{recipientfirst}} {{recipientsecond}}  \n"+
+                "{{recipientaddress1}}  \n" +
+                "{{recipientaddress2}}\n\n" +
+                "Dear {{msmrs}} {{recipientsecond}},\n\n" +
+                "Lorem ipsum dolor sit amet, eu __inani__ _{{data}}_ dolores dissentiunt usu, eu has eius atqui, an cum quot soluta. Cu oportere splendide eam, rebum inermis expetenda id his, eu veritus detraxit delicata vix. At legere definiebas usu. Eam eius erant possit ea. Eripuit habemus deseruisse ne mei, ex civibus molestie vim. Quo ei diam fabellas vivendum. Eu luptatum probatus duo.\n\n" +
+                "Per summo commune fabellas ex, saperet mediocrem ex nam, ad graeco docendi honestatis pri. An prima meliore eleifend vim, ea minim voluptaria complectitur mel. Qui nemore essent maiestatis in, eu impedit alienum pro. An mazim suscipiantur contentiones sea. Eu ceteros suscipit qualisque has, odio nominavi erroribus ius in.\n\n" +
+                "#HEADING -- TABLE DATA BELOW\n\n" +
+                "| Tables        | Are           | Cool  |\n"+
+                "| ------------- |:-------------:| -----:|\n"+
+                "| **col 3 is**  | right-aligned | $1600 |\n"+
+                "| col 2 is      | *centered*    |   $12 |\n"+
+                "| zebra stripes | ~~are neat~~  |    $1 |\n"
+
+            ;
+            var h = converter.makeHtml(templateText);
             var element = $('#example3div');
             element.html(h);
             $compile(element.contents())($scope);
-
-        }, function errorCallback(response) {
-            if (request.status === 0)
-            {
-                alert('Not connected. Verify Network.');
-            }
-            else if (request.status == 404)
-            {
-                alert('Requested page not found. [404]');
-            }
-            else if (request.status == 500)
-            {
-                alert('Internal Server Error [500].');
-            }
-            else if (exception === 'parsererror')
-            {
-                alert('Requested JSON parse failed.');
-            }
-            else if (exception === 'timeout')
-            {
-                alert('Time out error.');
-            }
-            else if (exception === 'abort')
-            {
-                alert('Ajax request aborted.');
-            }
-            else
-            {
-                alert('Uncaught Error.n' + jqXHR.responseText);
-            }
+            $scope.$apply();
         });
-    });
-    $scope.myText = "HEADER 2";
-    $scope.data = 5;
-    $scope.someValue = 15;
-    $scope.myTitle = "BOBCAT OF COURSE";
-    $scope.renderdirective = false;
-    $rootScope.triggerRelink = function() {
-        $rootScope.$broadcast('myEventName')
-    }
+        $scope.data = 5;
+        $scope.name = "Bob Santos";
+        $scope.address1 = "123 Main St.";
+        $scope.address2 = "Sometown, NY 10001";
+        $scope.recipientfirst = "Sam";
+        $scope.recipientsecond = "Tarly";
+        $scope.recipientaddress1 = "666 Elm St.";
+        $scope.recipientaddress2 = "Someothertown, NJ 07777";
+        $scope.msmrs = "Mr.";
+
     }
 );
 
@@ -68,7 +54,7 @@ angular.module('myApp').directive('vk', ['$compile', function ($compile) {
     return {
         template: '<div>Content of vk {{someValue}}</div>',
         restrict: 'E',
-        link: function(scope, element, attrs){
+        link: function (scope, element, attrs) {
             console.log('"link" function inside directive vk called, "element" param is: ', element)
         }
     };
@@ -76,13 +62,13 @@ angular.module('myApp').directive('vk', ['$compile', function ($compile) {
 
 angular.module('myApp')
     .directive('compile', ['$compile', function ($compile) {
-        return function(scope, element, attrs) {
+        return function (scope, element, attrs) {
             scope.$watch(
-                function(scope) {
+                function (scope) {
                     // watch the 'compile' expression for changes
                     return scope.$eval(attrs.compile);
                 },
-                function(value) {
+                function (value) {
                     // when the 'compile' expression changes
                     // assign it into the current DOM
                     element.html(value);
